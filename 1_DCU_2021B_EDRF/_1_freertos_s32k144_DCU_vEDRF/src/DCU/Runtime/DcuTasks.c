82725411116ef8ca95a085414bf72a500bb5c525
EfrenDRF <efren.delrealf@gmail.com>
2021-10-23 00:59:18 -0500

    DCU_SW_components integrated sucesfully

---
+++ 1_DCU_2021B_EDRF/_1_freertos_s32k144_DCU_vEDRF/src/DCU/Runtime/DcuTasks.c
@@ -0,0 +1,83 @@
+/*
+ * Tasks.c
+ *
+ *  Created on: 8 sep. 2021
+ *      Author: uid87544
+ */
+
+/* Kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
+
+/* SDK includes. */
+#include "sdk_project_config.h"
+
+
+#include "BasicTypes.h"
+#include "Window.h"
+#include "Door.h"
+#include "Button.h"
+#include "DoorApp.h"
+#include "WindowApp.h"
+#include "Wdg.h"
+#include "Mcu.h"
+#include "Mpu.h"
+#include "Adc.h"
+#include "Dio.h"
+#include "DcuTasks.h"
+
+/* Local Function Prototypes */
+void Tasks_StartOS(void);
+
+
+/* ============================================================================
+ * Function Name:
+ * Description:
+ * Arguments:
+ * Return:
+ * ========================================================================= */
+void app_task_200ms( void *pvParameters )
+{
+	TickType_t xNextWakeTime;
+
+	/* Casting pvParameters to void because it is unused */
+	(void)pvParameters;
+
+	/* Initialize xNextWakeTime - this only needs to be done once. */
+	xNextWakeTime = xTaskGetTickCount();
+
+	for( ;; )
+	{
+
+		/* Place this task in the blocked state until it is time to run again.
+		The block time is specified in ticks, the constant used converts ticks
+		to ms.  While in the Blocked state this task will not consume any CPU
+		time. */
+		vTaskDelayUntil( &xNextWakeTime, 200 );
+
+	}
+}
+
+void Tasks_StartOS(void)
+{
+	xTaskCreate(app_task_200ms,        "App200ms",         configMINIMAL_STACK_SIZE, NULL,  5, NULL);
+
+	Mpu_Init();
+
+	vTaskStartScheduler();
+
+}
+
+void init_hook(void) {
+
+    Mcu_Init();
+
+	Wdg_Init();
+
+    Dio_Init();
+
+    Adc_Init();
+
+	Tasks_StartOS();
+}
