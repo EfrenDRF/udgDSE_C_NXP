514e10daaace519b020e1ecd434d5b8ed7ee51c0
EfrenDRF <efren.delrealf@gmail.com>
2021-10-23 09:35:02 -0500

    MPU config

--- 1_DCU_2021B_EDRF/_1_freertos_s32k144_DCU_vEDRF/src/DCU/Microcontroller/Mcu/Mcu.c
+++ 1_DCU_2021B_EDRF/_1_freertos_s32k144_DCU_vEDRF/src/DCU/Microcontroller/Mcu/Mcu.c
@@ -7,10 +7,11 @@
 
 #include "BasicTypes.h"
 #include "Mcu.h"
-
+#include "sdk_project_config.h"
 
 void Mcu_Init(void)
 {
-
+    (void) CLOCK_SYS_Init(g_clockManConfigsArr, CLOCK_MANAGER_CONFIG_CNT, g_clockManCallbacksArr, CLOCK_MANAGER_CALLBACK_CNT);
+    (void) CLOCK_SYS_UpdateConfiguration(0U, CLOCK_MANAGER_POLICY_AGREEMENT);
 }
 
